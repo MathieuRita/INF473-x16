529b3a501e726cf16516679192a04e28668c035a
Johanna <johanna.jussey@polytechnique.edu>
2018-02-23 15:52:16 +0100

    Clean

--- build-Elements-Desktop_Qt_5_9_3_GCC_64bit-Default/Colorizer/CMakeFiles/SEColorizer_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "SEColorizer_automoc.cpp"
-  "CMakeFiles/SEColorizer_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/SEColorizer_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
